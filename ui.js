a494c7b13df67084b51a77714c00498b847b8cca
Dudemullet <elpaip@gmail.com>
2014-11-26 09:16:28 -0800

    adds reference to movies and keeps them in memory

--- ui.js
+++ ui.js
@@ -1,9 +1,14 @@
 (function(){
   var recentMoviesTemplate = Handlebars.compile($("#movies-table-template").html());
   var $table = $("table");
+  var movies = [];
 
   var displayMovies = function(data) {
-    var movies = data.results;
+    movies = data.results;
+    movies = movies.map(function(v,i){
+      v["index"] = i;
+      return v;
+    });
     var moviesHTML = recentMoviesTemplate({movie: movies});
     $table.find("tbody").append(moviesHTML);
   }
