c86b7c74c1b8789232baa429e674d4ac496f5774
Dudemullet <elpaip@gmail.com>
2014-11-26 09:38:33 -0800

    Adds sort

--- ui.js
+++ ui.js
@@ -3,14 +3,37 @@
   var $table = $("table");
   var movies = [];
 
+  var addIndexes = function(value, index) {
+      value["index"] = index;
+      return value;
+  }
+
   var displayMovies = function(data) {
     movies = data.results;
-    movies = movies.map(function(v,i){
-      v["index"] = i;
-      return v;
-    });
+    movies = movies.map(addIndexes);
     var moviesHTML = recentMoviesTemplate({movie: movies});
     $table.find("tbody").append(moviesHTML);
+  }
+
+  $("#recent-movies").on("click", "th", function(e){
+    var sortField = $(this).data("sort");
+    var sortedMovies = sortBy(sortField, movies);
+
+    // display These
+    console.log(sortedMovies);
+  });
+
+  var sortBy = function(field, array) {
+   return array.sort(function(a,b){
+      if (a[field] > b[field]) {
+        return 1;
+      }
+      if (a[field] < b[field]) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
   }
 
   API.getLatestMovies(displayMovies);
